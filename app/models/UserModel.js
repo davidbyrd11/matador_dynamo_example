f7540670a21f70bcb662742fef0c3d2d00444c0f
David Byrd <david@byrdhou.se>
2012-11-23 04:12:31 -1000

    finished unit tests

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -1,13 +1,35 @@
 module.exports = function (app, config) {
 
   return app.getModel("Application", true).extend(function(){
-
+    this.users = this.db.get('users');
   })
   .methods({
-    getNumTables: function(callback) {
-      var T = this;
-      this.db.fetch(function(err){
-        callback(err, Object.keys(T.db.tables).length);
+    create: function (user, callback) {
+      if(user.email === undefined) throw "Users must have emails!";
+      user.UserID = user.email;
+      this.users.put(user).save(callback);
+    },
+    find: function (email, callback) {
+      var user = this.users.get({"UserID": email});
+      user.fetch(function(err, data) {
+        if(err) throw err;
+        callback(err, data);
+      });
+    },
+    update: function (email, updates, callback) {
+      var user = this.users.get({"UserID": email});
+      user.update(function(){
+        for(var key in updates) {
+          var attribute = updates[key];
+          this.put(key, attribute);
+        }
+      }).save(callback);
+    },
+    destroy: function (email, callback) {
+      var user = this.users.get({"UserID": email});
+      user.destroy(function(err){
+        if(err) throw err;
+        callback(err);
       });
     }
   });
--- test.js
+++ test.js
@@ -1,9 +1,57 @@
 module.exports = function (app, config) {
   console.log('Torero is testing the strength of the bull ...');
-  var assert = require('assert');
-
-  app.getModel("User", false).getNumTables(function(err, num){
-    assert(num === 5, "There are 5 tables");
-  });
-
+  var assert = require('assert'),
+      async = require('async'),
+      updates = {name: "David Michael Byrd"},
+      mock_user = {
+    "name": "David Byrd",
+    "email": "david@byrdhou.se",
+    "twitter": "davidbyrd11",
+    "address": "123 Happy St. New York, NY 10003 Apt 99999",
+    "access": "admin"
+  };
+  async.series([
+    function (cback){
+      app.getModel("User", false).create(mock_user, function(err){
+        assert(err === undefined, "User created successfully");
+        if(err) throw err;
+        cback(null);
+      });
+    },
+    function (cback) {
+      app.getModel("User", false).find(mock_user.email, function(err, retrieved_user){
+        if(err) throw err;
+        assert(retrieved_user.UserID === mock_user.email, 'UserID is correct');
+        assert(retrieved_user.UserID === retrieved_user.email, 'UserID is the user\'s email');
+        assert(retrieved_user.name === mock_user.name, 'name is correct');
+        assert(retrieved_user.address === mock_user.address, 'address is correct');
+        assert(retrieved_user.twitter === mock_user.twitter, 'twitter handle is correct');
+        assert(retrieved_user.access  === mock_user.access, 'access is correct');
+        assert(retrieved_user.email === mock_user.email, 'email is correct');
+        cback(null);
+      });
+    },
+    function (cback) {
+      app.getModel("User", false).update(mock_user.email, updates, function(err){
+        if(err) throw err;
+        cback(null);
+      });
+      
+    },
+    function (cback) {
+      app.getModel("User", false).find(mock_user.email, function(err, updated_user){
+        if(err) throw err;
+        cback(null);
+        assert(updated_user.name === updates.name, "User was properly updated");
+      });
+    },
+    function (cback) {
+      app.getModel("User", false).destroy(mock_user.email, function(err){
+        if(err) throw err;
+        cback(null);
+      });
+    }],
+    function (err, results) {
+      if(err) throw err;
+    });
 };
